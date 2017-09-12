b4701db04d2c71ceddf2a9b86b806ef15b331e25
Juan <jdcb1218@gmail.com>
2017-09-12 11:45:05 -0500

    ADS Relase stable

--- sites/all/modules/custom/agenda/theme/operations-node-availability.tpl.php
+++ sites/all/modules/custom/agenda/theme/operations-node-availability.tpl.php
@@ -1,14 +1,24 @@
+<div class="operation-node">
+<?php ctools_include('modal'); ctools_include('ajax'); ?>
+
+<div class="create meeting">
 <?php 
+print(l(t('Create meeting'), 'meeting-new/'.arg(1).'/nojs', 
+      array('attributes' => array('class' => 'ctools-use-modal'))));
+?>
+</div>
 
 
-ctools_include('modal');
-ctools_include('ajax');
-
+<div class="edit availability">
+<?php 
 print(l(t('Edit availability'), 'availability-edit/'.arg(1).'/nojs', 
       array('attributes' => array('class' => 'ctools-use-modal'))));
-
-
+?>
+</div>
+<div class="delete availability">
+<?php 
 print(l(t('Delete availability'), 'availability-delete/'.arg(1).'/nojs', 
       array('attributes' => array('class' => 'ctools-use-modal'))));
-
 ?>
+</div>
+</div>

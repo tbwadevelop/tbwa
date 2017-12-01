a7af1957226e9d85922cfa61a2838df76470dd28
Juan <jdcb1218@gmail.com>
2017-11-30 19:44:12 -0500

    update codec

--- sites/all/themes/custom/andes/js/script.js
+++ sites/all/themes/custom/andes/js/script.js
@@ -1,11 +1,11 @@
 (function ($) {
   Drupal.behaviors.agenda = {
     attach: function (context, settings) {
-    	$('.page-user-login #edit-name').attr('placeholder', 'Usuario');
-    	$('.page-user-login #edit-pass').attr('placeholder', 'Clave');
-        $( "#edit-submit-usuarios-agenda-de-servicios" ).click(function() {
-		$("#block-agenda-citas-ajax").css("display", "initial");
-		});
+		    $('.page-user-login #edit-name').attr('placeholder', 'Usuario');
+	    	$('.page-user-login #edit-pass').attr('placeholder', 'Clave');
+	        $( "#edit-submit-usuarios-agenda-de-servicios" ).click(function() {
+			$("#block-agenda-citas-ajax").css("display", "initial");
+    	});
     }
   };
 }(jQuery));

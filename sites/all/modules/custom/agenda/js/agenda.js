f1edce50ef3edb0b1f81621a9e9dee21bf25f2d0
Juan <jdcb1218@gmail.com>
2017-12-11 21:54:13 -0500

    saved users form

--- sites/all/modules/custom/agenda/js/agenda.js
+++ sites/all/modules/custom/agenda/js/agenda.js
@@ -4,7 +4,7 @@
     	var count = $('.form-group .form-inline.form-wrapper.form-group').length;
     	$.post( "/crear/"+settings.agenda.nodo+"/citas", function( data ) {
 			if ($(context).context) {
-				if ($(context).context.id == 'agenda-form-records') {
+				if ($(context).context.id == 'agenda-records-form') {
 					$('.form-item-field-container-'+count+'-field1 input').val($(".views-row-last .views-field-name").text());
 					$('.form-item-field-container-'+count+'-field2 input').val($(".views-row-last .views-field-field-codigo").text());
 					$('.form-item-field-container-'+count+'-field3 input').val($(".views-row-last .views-field-mail").text()); 						

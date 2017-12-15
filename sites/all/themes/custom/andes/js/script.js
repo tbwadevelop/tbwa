0bed510827d228d597f8363b3dd325bc672506a4
Juan <jdcb1218@gmail.com>
2017-12-15 14:56:16 -0500

    js modal and module node title

--- sites/all/themes/custom/andes/js/script.js
+++ sites/all/themes/custom/andes/js/script.js
@@ -1,6 +1,8 @@
 (function ($) {
 	Drupal.behaviors.agenda = {
 	    attach: function (context, settings) {
+	    	$(window).load(function(){ $('#dvLoading').fadeOut(2000);});
+	    	$(window).load(function(){ $('.ctools-modal-loading').fadeOut(2000);});
 			$('.page-user-login #edit-name').attr('placeholder', 'Usuario');
 	    	$('.page-user-login #edit-pass').attr('placeholder', 'Clave');
 	        $( "#edit-submit-usuarios-agenda-de-servicios" ).click(function() {

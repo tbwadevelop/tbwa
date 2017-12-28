220e1be16b6413019acc7c5bf859ec2f18fba440
Juan <jdcb1218@gmail.com>
2017-12-27 19:08:12 -0500

    user edit profile

--- sites/all/themes/custom/andes/js/script.js
+++ sites/all/themes/custom/andes/js/script.js
@@ -1,6 +1,13 @@
 (function ($) {
 	Drupal.behaviors.agenda = {
 	    attach: function (context, settings) {
+
+	    	// Page Edit user.
+	    	$('.page-user-edit #block-system-main #user-profile-form #edit-locale').remove();
+	    	$(".page-user-edit #user-profile-form .form-wrapper.form-group").addClass("col-lg-4 col-xs-6 col-sm-6");
+
+
+
 	    	$(window).load(function(){ $('#dvLoading').fadeOut(2000);});
 	    	$(window).load(function(){ $('.ctools-modal-loading').fadeOut(2000);});
 			$('.page-user-login #edit-name').attr('placeholder', 'Usuario');

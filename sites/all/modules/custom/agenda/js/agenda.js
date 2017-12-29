d093f43ea01c70c36acf8ec507a39cb0c851e688
Juan <jdcb1218@gmail.com>
2017-12-28 22:00:54 -0500

    add funcionality consulta motivo

--- sites/all/modules/custom/agenda/js/agenda.js
+++ sites/all/modules/custom/agenda/js/agenda.js
@@ -5,10 +5,11 @@
     	$.post( "/crear/"+settings.agenda.nodo+"/citas", function( data ) {
 			if ($(context).context) {
 				if ($(context).context.id == 'agenda-records-form') {
-					$('.form-item-field-container-'+count+'-uid input').val($(".view-id-usuarios_agenda_de_servicios .views-row-last .views-field-uid").text());
-					$('.form-item-field-container-'+count+'-name input').val($(".view-id-usuarios_agenda_de_servicios .views-row-last .views-field-name").text());
-					$('.form-item-field-container-'+count+'-codec input').val($(".view-id-usuarios_agenda_de_servicios .views-row-last .views-field-field-codigo").text());
-					$('.form-item-field-container-'+count+'-email input').val($(".view-id-usuarios_agenda_de_servicios .views-row-last .views-field-mail").text()); 						
+					$('.form-item-field-container-'+count+'-uid input').val($(".view-id-usuarios_agenda_de_servicios .views-row-last .views-field-uid .field-content").text());
+					$('.form-item-field-container-'+count+'-name input').val($(".view-id-usuarios_agenda_de_servicios .views-row-last .views-field-name .field-content").text());
+					$('.form-item-field-container-'+count+'-codec input').val($(".view-id-usuarios_agenda_de_servicios .views-row-last .views-field-field-codigo .field-content").text());
+					$('.form-item-field-container-'+count+'-email input').val($(".view-id-usuarios_agenda_de_servicios .views-row-last .views-field-mail .field-content").text()); 						
+			        $('.form-item-field-container-'+count+'-motivo input').val($('select[name=selector-motivo]').val());  						
 				}
 		  	}
 		});

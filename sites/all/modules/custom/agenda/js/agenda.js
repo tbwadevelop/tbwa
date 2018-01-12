45241356505ce7e2c8680b8b2e400e95726ccd8a
Danilo Santos <johndoe@example.com>
2018-01-11 19:15:21 -0500

    agenda.js

--- sites/all/modules/custom/agenda/js/agenda.js
+++ sites/all/modules/custom/agenda/js/agenda.js
@@ -1,8 +1,8 @@
 (function($) {
   Drupal.behaviors.agenda = {
     attach: function (context, settings) {
-    	var count = $('.container-inline.container.form-wrapper.form-group').length;
-    	$.post( "/crear/"+settings.agenda.nodo+"/citas", function( data ) {
+		var count = $('.container-inline.container.form-wrapper.form-group').length;
+    	$.post(window.location.pathname, function( data ) {
 			if ($(context).context) {
 				if ($(context).context.id == 'agenda-records-form') {
 					$('.form-item-field-container-'+count+'-uid input').val($(".view-id-usuarios_agenda_de_servicios .views-row-last .views-field-uid .field-content").text());

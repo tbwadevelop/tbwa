e58da537ff722c93517dc1a95d141e83a94fc197
Juan <jdcb1218@gmail.com>
2018-01-16 17:43:17 -0500

    container access login name

--- sites/all/modules/custom/agenda/js/request.js
+++ sites/all/modules/custom/agenda/js/request.js
@@ -1,6 +1,10 @@
 (function ($) {
 	Drupal.behaviors.request = {
 	    attach: function (context, settings) {
+
+	  	$('.view-id-user_login .views-field-name .field-content').each(function (index, value) { 
+		  $("<h2> "+$('.view-id-user_login .views-label.views-label-name').text()+" "+$(this).context.innerText+"</h2>" ).appendTo( ".date-views-pager.clearfix.date-nav-wrapper" );
+		});
 	     	$('.mini tr').each(function(){
 	     		$(this).find('td').each(function(){
 	     			var res = $(this).context.id.split("-");

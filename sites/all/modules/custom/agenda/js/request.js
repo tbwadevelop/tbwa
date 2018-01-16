f34067c6b58d647237033d02338ccf2f9dea2311
Juan <jdcb1218@gmail.com>
2018-01-16 10:31:09 -0500

    Fixed codec update

--- sites/all/modules/custom/agenda/js/request.js
+++ sites/all/modules/custom/agenda/js/request.js
@@ -1,7 +1,12 @@
-(function($) {
-
-	$.post( "/crear/125/citas", function( data ) {
-		console.log("post");
-	});
-
-})(jQuery);
+(function ($) {
+	Drupal.behaviors.request = {
+	    attach: function (context, settings) {
+	     	$('.mini tr').each(function(){
+	     		$(this).find('td').each(function(){
+	     			var res = $(this).context.id.split("-");
+	     			$(this).context.innerHTML = '<div class="month mini-day-on"> <a href="'+Drupal.settings.agenda.dominio+'/calendario-consejero/diario/'+res[1]+'-'+res[2]+'-'+res[3]+'/'+res[3]+'">'+$(this).context.innerText+'</a></div>';
+			     })
+			 })
+	    }
+  	};
+}(jQuery));

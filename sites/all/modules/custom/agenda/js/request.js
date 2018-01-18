92687612399380401234b11a8769df838bf9051c
jdcb1218 <jdcb1218@gmail.com>
2018-01-18 18:09:48 -0500

    update js

--- sites/all/modules/custom/agenda/js/request.js
+++ sites/all/modules/custom/agenda/js/request.js
@@ -2,15 +2,24 @@
 	Drupal.behaviors.request = {
 	    attach: function (context, settings) {
 
-	  	$('.view-id-user_login .views-field-name .field-content').each(function (index, value) { 
-		  $("<h2> "+$('.view-id-user_login .views-label.views-label-name').text()+" "+$(this).context.innerText+"</h2>" ).appendTo( ".date-views-pager.clearfix.date-nav-wrapper" );
+		if ($(".main-container .date-views-pager h2").length == false){
+		  	$('.view-id-user_login .views-field-name .field-content').each(function (index, value) { 
+			  $("<h2> "+$('.view-id-user_login .views-label.views-label-name').text()+" "+$(this).context.innerText+"</h2>" ).appendTo(".view-display-id-page_3 .date-views-pager.clearfix.date-nav-wrapper");
+			});
+		}
+
+		$( ".date-views-pager h3 a" ).attr( "src", function() {
+			var params = $(this).context.href.split("-");
+		 	var arg = params[1].split("/");
+		 	$('.date-views-pager h3 a').text($( ".date-views-pager h3 a" ).text() + ' '+ arg[2]);
 		});
-	     	$('.mini tr').each(function(){
-	     		$(this).find('td').each(function(){
-	     			var res = $(this).context.id.split("-");
-	     			$(this).context.innerHTML = '<div class="month mini-day-on"> <a href="'+Drupal.settings.agenda.dominio+'/calendario-consejero/diario/'+res[1]+'-'+res[2]+'-'+res[3]+'/'+res[3]+'">'+$(this).context.innerText+'</a></div>';
-			     })
-			 })
+
+     	$('.mini tr').each(function(){
+     		$(this).find('td').each(function(){
+     			var res = $(this).context.id.split("-");
+     			$(this).context.innerHTML = '<div class="month mini-day-on"> <a href="'+Drupal.settings.agenda.dominio+'/calendario-consejero/diario/'+res[1]+'-'+res[2]+'-'+res[3]+'/'+res[3]+'">'+$(this).context.innerText+'</a></div>';
+		     })
+		 })
 	    }
   	};
 }(jQuery));

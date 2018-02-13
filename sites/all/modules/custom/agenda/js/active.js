267276b54bdcaf0bb15579d97709170f00d8950d
Juan <jdcb1218@gmail.com>
2018-02-13 17:42:26 -0500

    active.js

--- sites/all/modules/custom/agenda/js/active.js
+++ sites/all/modules/custom/agenda/js/active.js
@@ -1,6 +1,24 @@
 (function ($) {
-	Drupal.behaviors.request = {
-	    attach: function (context, settings) {
-  		console.log("active.js");
-  	};
-}(jQuery);	    	
+  Drupal.behaviors.active = {
+    attach: function (context, settings) {
+     // Rol Consejero //
+	     // Active views calendar 
+	     $(".page-calendario-consejero-diario .menu.nav li:nth-child(1)").addClass("active-trail active");
+	     $(".page-calendario-consejero-semanal .menu.nav li:nth-child(1)").addClass("active-trail active");
+	     $(".page-calendario-consejero-mes .menu.nav li:nth-child(1)").addClass("active-trail active");
+	     // Active historial cita 
+	     $(".page-node-edit.node-type-disponibilidad .menu.nav li:nth-child(2)").addClass("active-trail active");
+	     // Add cita 
+	     $(".page-crear-citas .menu.nav li:nth-child(1)").addClass("active-trail active");
+
+      //Rol estudiante
+       // Interna de servicios.
+		 if ( $("#block-views-servicios-block-1").length > 0 ) {
+			$(".page-calendario-estudiante .menu.nav li:nth-child(1)").addClass("active-trail active");	
+		 }
+       // Mis datos	
+		 $(".page-user-edit .menu.nav li:nth-child(2)").addClass("active-trail active");
+	
+    }
+  };
+}(jQuery));

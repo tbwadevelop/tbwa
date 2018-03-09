365f14ab8f60e1619317306adb15d682dbb7ccc4
Juan <jdcb1218@gmail.com>
2018-03-09 12:12:03 -0500

    upadate core modules

--- sites/all/modules/custom/agenda/js/request.js
+++ sites/all/modules/custom/agenda/js/request.js
@@ -5,17 +5,13 @@
 	    	$('.page-user-edit #block-system-main #user-profile-form #edit-locale').remove();
 	    	$(".page-user-edit #user-profile-form .form-wrapper.form-group").addClass("col-lg-4 col-xs-6 col-sm-6");
 
-	    	//class de contenedor de todas las clases
-	    	$('body.not-front .main-container').removeClass("container");
-	    	$('body.not-front .main-container').addClass("container-fluid");
-
-	    	//contenerdor de consejero home 
+	    	//contenerdor de consejero home // change dimencion table
 	    	$('body.page-calendario-consejero.page-calendario-consejero-diario aside').removeClass("col-sm-3");
 	    	$('body.page-calendario-consejero.page-calendario-consejero-diario aside').addClass("col-lg-3 col-md-3 col-sm-12 col-xs-12");
 	    	$('body.page-calendario-consejero.page-calendario-consejero-diario .row > section').removeClass("col-sm-9");
 	    	$('body.page-calendario-consejero.page-calendario-consejero-diario .row > section').addClass("col-lg-9 col-md-9 col-sm-12 col-xs-12");
 
-	    	//contenerdor de estudiante home 
+	    	// //contenerdor de estudiante home 
 	    	$('body.page-calendario-estudiante aside').removeClass("col-sm-3");
 	    	$('body.page-calendario-estudiante aside').addClass("col-lg-3 col-md-3 col-sm-12 col-xs-12");
 	    	$('body.page-calendario-estudiante .row > section').removeClass("col-sm-9");
